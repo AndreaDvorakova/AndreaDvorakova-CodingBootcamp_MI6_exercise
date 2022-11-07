33b4733cf0268ccf807b90f49f5580f0c982b65c
AndreaDvorakova <csajka.andi@gmail.com>
2022-11-07 12:00:33 +0100

    gitignore for images and relations

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Status extends Model
 {
     use HasFactory;
+
+    public function people()
+    {
+        return $this->hasMany(People::class);
+    }
 }
